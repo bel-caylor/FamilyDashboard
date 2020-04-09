e193e900fe72600b51a1ab7447f13bdb57237625
Belinda Caylor <belinda.caylor@gmail.com>
2020-04-09 15:04:48 -0500

    Create Login Form

---
+++ public/login.php
@@ -0,0 +1,122 @@
+<!-- Need to troubleshoot password. -->
+
+<?php require_once('../private/initialize.php');
+$step = "1-email";
+$stepID = $_POST['StepID'] ?? '';
+   if($_SERVER['REQUEST_METHOD'] == 'POST') {
+     $_SESSION['email'] = $_POST['email'] ?? $_SESSION['email'];
+     if ($_SESSION['email'] != '') {
+       switch ($stepID) {
+         case '1':  //Enter Email
+            $results = sqlSelect("family-members", "ID", "ASC", "Email", $_SESSION['email']);
+          //No DATA
+            if ($results == 'No data returned.') {
+              $step = '2-createFamily';
+              break;}
+          //Set $_SESSION VALUES
+            //Create users array
+            while($row = mysqli_fetch_assoc($results)) {
+              $_SESSION['users'][$row['Name']] = $row;
+            };
+            // $_SESSION['users'] = $results;
+            $users = $_SESSION['users'];
+            $userNames = array_keys($_SESSION['users']);
+            $_SESSION['familyID'] = $_SESSION['users'][$userNames[0]]['Family_ID'];
+            $family = mysqli_fetch_assoc(sqlSelect("family", "ID", "ASC", "ID", $_SESSION['familyID']));
+            $_SESSION['family'] = $family['Family'];
+            $_SESSION['postalCode'] =$family['Postal_Code'];
+          //Multipule Users
+            if (mysqli_num_rows($results) > 1) {
+              $step = '3-selectUser';
+              break;}
+          //One User
+            $step = '4-password';
+            break;
+
+         case '3':  //Select User
+           $_SESSION['currentUserID'] = $_POST['User'];
+           $user = mysqli_fetch_assoc(sqlSelect("family-members", "ID", "ASC", "ID", $_SESSION['currentUserID']));
+           $_SESSION['currentName'] = $user['Name'];
+           $_SESSION['password'] = $user['hashed_password'];
+           $step = '4-password';
+           break;
+         case '4':
+            // $pass = password_verify($_POST['password'], $_SESSION['password']);
+            // echo $pass;
+            if(password_verify($_POST['password'], $_SESSION['password']) == 0) {
+              header("Location: " . WWW_ROOT . "/familySetup.php");
+              exit;
+            }else {  //Invalid Password
+              $step = '5-invalid';
+            };
+
+       }
+       //Check for Users with that Email
+     }
+   }
+   $stepID = substr($step, 0, 1)
+?>
+<?php include(SHARED_PATH . '/header.php') ?>
+<body>
+  <header class="section">
+    Family Dashboard LOGIN
+  </header>
+  <main>
+  <!-- EMAIL -->
+    <h2 class="section"><span <?php if ($stepID>2) {echo " class='hidden'";} ?>>Enter&nbsp;</span>E-mail</h2>
+    <form class="form" action="<?php echo WWW_ROOT?>/login.php" method="post">
+      <input type="email" name="email" placeholder="Email Address" value="<?php echo $_SESSION['email'] ?>" <?php if ($stepID>2) {echo " disabled";} ?>>
+      <input type="hidden" name="StepID" value="1">
+      <input <?php if ($stepID>2) {echo "class=\"hidden\" ";} ?> type="submit" value="Submit">
+    </form>
+
+  <!-- CREATE FAMILY -->
+    <div class="status-message" <?php if ($stepID != 2) {echo " hidden";} ?>>
+      <p>E-mail doesn't exist.  Do you want to create a NEW Family?</p>
+      <p><a href="<?php echo WWW_ROOT ?>/familySetup.php">Create Family</a></p>
+    </div>
+
+  <!-- SELECT USER -->
+    <div <?php if ($stepID != 3) {echo " hidden";} ?>>
+      <h2 class="section">Select User</h2>
+      <form class="form" action="<?php echo WWW_ROOT?>/login.php?" method="post">
+        <?php
+          if (mysqli_num_rows($results) > 0) {
+            $count = 1;
+            $checked = " checked";
+            foreach($userNames as $name) {
+              if ($count > 1) {
+                $checked = "";
+              };
+              echo "<input type='radio' id='" . $users[$name]['ID'] . "' name='User' value='" . $users[$name]['ID'] . "' " . $checked .  ">";
+              echo "<label for='" . $name . "'>" . $name . "</label><br>";
+              $count = $count + 1;
+            }
+            // while($row = mysqli_fetch_assoc($users)) {
+            //
+            // }
+          }
+        ?>
+        <input type="hidden" name="StepID" value="3">
+        <input type="submit" value="Submit" <?php if ($stepID != 3) {echo " hidden";} ?>>
+      </form>
+    </div>
+
+  <!-- ENTER PASSWORD  -->
+    <div <?php if ($stepID > 5) {echo " hidden";} ?>>
+      <h2 class="section">Enter Password</h2>
+      <form class="form" action="<?php echo WWW_ROOT?>/login.php" method="post">
+        <input type="password" name="password" placeholder="password">
+        <input type="hidden" name="StepID" value="4">
+        <input type="submit" value="Submit">
+      </form>
+    </div>
+
+    <!-- CREATE FAMILY -->
+      <div class="form status-message" <?php if ($stepID != 5) {echo " hidden";} ?>>
+        <p>Invalid Password</p>
+        <p><a href="<?php echo WWW_ROOT ?>/familySetup.php">Create Family</a></p>
+      </div>
+
+  </main>
+</body>
