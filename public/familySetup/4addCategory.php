6258fe116a957c1ccc11a98e2a90507d22e1dbdb
Belinda Caylor <belinda.caylor@gmail.com>
2020-04-20 08:05:19 -0500

    Check for dubplicate category in step 4.

--- public/familySetup/4addCategory.php
+++ public/familySetup/4addCategory.php
@@ -4,11 +4,21 @@
   $_SESSION['step4Msgs'] = array();
 
   //Add Room name to category-names.
-  sqlAddCategory($_POST['category'], $_POST['catName']);
 
-  //Add Default Tasks to tasks.
-  sqlAddDefaultTasks($_POST['category']);
-  $_SESSION['step'] = '4';
+  //Check for duplicate Category Name.
+
+  $catNameID = sqlAddCategory($_POST['category'], $_POST['catName'], 1);
+  // echo $catNameID . "<br>";
+  if ($catNameID == "insert failed") {
+    echo 'insert failed';
+  } else {
+    // echo '<br>' . $catNameID;
+
+    //Add Default Tasks to tasks.
+    sqlAddDefaultTasks($_POST['category'], $catNameID);
+    $_SESSION['step'] = '4';
+  }
+
 
   header("Location: " . WWW_ROOT . "/familySetup.php");  //REDIRECT
 
