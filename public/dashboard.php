62e4918026da8be05170b72b16f9de29b486e298
Belinda Caylor <belinda.caylor@gmail.com>
2020-04-21 10:31:23 -0500

    create dashboard layout

---
+++ public/dashboard.php
@@ -0,0 +1,54 @@
+<?php require_once('../private/initialize.php'); ?>
+
+<?php ?>
+
+<?php include(SHARED_PATH . '/header.php') ?>
+<body>
+  <header>
+      <?php echo $_SESSION['currentName'] . "`s Dashboard<br>";?>
+  </header>
+
+  <main>
+    <br>
+<!-- Challenge Report Section -->
+    <div class="section inline">
+      <button onclick="clickExpandBtn('reports')">
+        <h2 id="reports" class="inline">&#9660; Challenge Reports</h2>
+      </button>
+    </div>
+
+<!-- Complete Task Section -->
+    <div class="section inline">
+      <button onclick="clickExpandBtn('completeTasks')">
+        <h2 id="reports" class="inline">&#9660; Complete Tasks</h2>
+      </button>
+    </div>
+    <?php include(PUBLIC_PATH . '\dashboard\tblCompleteTasks.php') ?>
+
+
+<!-- Admin Grade Task Section -->
+    <div class="section inline">
+      <button onclick="clickExpandBtn('gradeTasks')">
+        <h2 id="reports" class="inline">&#9660; Grade Tasks</h2>
+      </button>
+    </div>
+
+<!-- Admin Assign Task Section -->
+    <div class="section inline">
+      <button onclick="clickExpandBtn('assignTasks')">
+        <h2 id="reports" class="inline">&#9660; Assign Tasks</h2>
+      </button>
+    </div>
+
+<!-- Go to familySetup -->
+    <div class="section inline">
+      <button onclick="clickExpandBtn('familySetup')">
+        <h2 id="reports" class="inline">&#9660; Add/Remove Tasks, Categories, Users</h2>
+      </button>
+    </div>
+
+</section>
+
+
+  </main>
+</body>
