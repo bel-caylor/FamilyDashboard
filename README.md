6d0fc2bfe8fd41dfbebe47df50f41bdf6a8c4f0a
Belinda Caylor <belinda.caylor@gmail.com>
2020-05-06 07:54:14 -0500

    update sessionValues

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
 #Family Dashboard
 
 ##To Do List
-- Need to think about how to handle $errors.
+- create dashboard page
+  - challenge/report section
+    - 7-Day House Minute chart.
+    - Personal Goals chart.
+  -complete tasks.
+    - change Time to complete
+-   - uncheck task complete box
+  - admin assign tasks table.
+    - include calculation of assigned tasks.
+  - admin grade tasks.
+    - add note to user.
+  - add/remove tasks.
+    - go to familySetup page.
+
+  -familySetup page
+    - calculate assigned tasks totals for users
+
+  -Add mysqli_real_escape_string to sql calls.
+  -Add '' to all sql inputs.
+
+
 
 ##Bugs
+- Fix validateUser
+    - seperate from sqlAddUser
+    - verify it works for Edit User
+- Fix password check at login
+- Need to think about how to handle $errors.
--- private/sessionValuesHTML.php
+++ private/sessionValuesHTML.php
@@ -1,20 +1,33 @@
 <?php require_once('initialize.php'); ?>
+<?php require_once('./shared/optionsFreq.php');
 
-  <?php
+  // echo $tasks;
+  // print_r($tasks);
+  // echo date(DATE_ATOM) . "<br>";
+  // // $date = date("Y-m-d H:i:s.Z");
+  // $date = date(DATE_ATOM, "2020-04-21");
+  // echo $date;
+  printValue('sql');
+
+
+  // echo ;
       // echo 'array_key_exists=' . array_key_exists("Bel",$_SESSION['users']) . '<br>';
       // $newArray = replaceKey($_SESSION['users'], "Bel", "Belinda");
       // printArray($newArray);
       // $_SESSION['users'] = replaceKey($_SESSION['users'], "Bel", "Belinda");
-      // $_SESSION['users'] = [];
-      // unset($_SESSION['users'][NULL]);
-      // json_change_key($_SESSION['users'], $_SESSION['users']['Bel'], $_SESSION['users']['Belinda']);
-      // unset( $_SESSION['users']['47']);
+      // $_SESSION['admin'] = 1;
+
+      // $_SESSION['step'] = 4;
+      // jshange_key($_SESSION['users'], $_SESSION['users']['Bel'], $_SESSION['users']['Belinda']);
+      // unset( $_SESSION['users']['1']);
       //Step1
-      array_push($_SESSION['step2Msgs'], "<br>Passwords don\'t match.");
+      // array_push($_SESSION['step2Msgs'], "<br>Passwords don\'t match.");
+// sqlCatNames($familyID);
+include(PRIVATE_PATH . '/shared/optionsCatName.php');
   ?>
 <div> <p>Step 1</p>
 <?php
-  // printValue('REQUEST_METHOD');
+  echo $_POST['REQUEST_METHOD'];
   printValue('step');
   printValue('familyID');
   printValue('family');
@@ -26,15 +39,40 @@
 
 <div> <p>Step 2</p>
   <?php
-  // echo count($_SESSION['step2Msgs']);
+  echo count($_SESSION['step2Msgs']) . "<br>";
+  printValue('email');
   printValue('aryUser');
   printValue('step2Msgs');
+  echo echoMsgArray($_SESSION['step2Msgs']);
   printValue('currentUserID');
   printValue('currentName');
+  printValue('admin');
   printValue('password');
   printValue('step3Msgs');
   printValue('input');
   printValue('users');
+
+  ?>
+</div>
+
+<div> <p>Step 4</p>
+  <?php
+    // $tasks = sqlTasks($_SESSION['familyID']);
+    // print_r(mysqli_fetch_assoc($tasks));
+    // echo '<br>';
+    // foreach (mysqli_fetch_assoc($tasks) as $task) {
+    //   echo $task;
+    // }
+    // printValue('aryUser');
+
+    // $freq = sqlSelect("frequency", "ID");
+    // while($row = mysqli_fetch_assoc($freq)) {
+    //   // array_push($_SESSION['freq'], $row);
+    //   $_SESSION['freq'][$row['ID']] = $row;
+    // }
+
+    // printValue('freq');
+    // optionFreq(2);
 
   ?>
 </div>
